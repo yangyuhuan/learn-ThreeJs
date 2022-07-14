cf9a2129c670879c629994a4af8950cce447ac86
yangyuhuan <526779045@qq.com>
2022-07-14 23:53:16 +0800

    add:basics-scene

---
+++ basics-scene/script.js
@@ -0,0 +1,30 @@
+console.log(THREE);
+
+const canvas = document.querySelector("canvas.webgl");
+const sizes = {
+  width: 800,
+  height: 600,
+};
+//scene
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+//camera
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+//渲染器 Renderer
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
