c41d9dbe144084925291db53d2ccaac3b58af9eb
yangyuhuan <526779045@qq.com>
2022-08-14 22:54:07 +0800

    feat: add three-basic

--- laochen-three/01-THREE-BASIC/src/main/main.js
+++ laochen-three/01-THREE-BASIC/src/main/main.js
@@ -1,11 +1,13 @@
 import * as THREE from "three";
 //导入轨道控制器
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+//导入动画库
+import gsap from "gsap";
 
-//目标：使用控制器查看3d物体
+//目标：掌握gsap设置各种动画效果
 
 //1.创建场景
-const scece = new THREE.Scene();
+const scene = new THREE.Scene();
 
 //2.创建相机
 const camera = new THREE.PerspectiveCamera(
@@ -17,7 +19,7 @@ const camera = new THREE.PerspectiveCamera(
 
 //设置相机的位置
 camera.position.set(0, 0, 10);
-scece.add(camera);
+scene.add(camera);
 
 //3.添加物体
 //创建几何体
@@ -25,8 +27,18 @@ const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 //根据几何体和材质创建物体
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+//修改物体的位置
+//cube.position.set(5, 0, 0);
+// cube.position.x = 3;
+//缩放
+//cube.scale.set(3, 2, 1);
+//cube.scale.x = 5;
+//旋转
+// cube.rotation.set(Math.PI / 4, 0, 0, "XZY");
+
 //将几何体添加到场景中
-scece.add(cube);
+scene.add(cube);
 
 //4.初始化渲染器
 const renderer = new THREE.WebGLRenderer();
@@ -36,13 +48,33 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //5.使用渲染器，通过相机将场景渲染进来
-//renderer.render(scece, camera);
+//renderer.render(scene, camera);
 
-//创建轨道控制器
+//6.创建轨道控制器
 const controls = new OrbitControls(camera, renderer.domElement);
 
+//7.添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+//设置时钟
+const clock = new THREE.Clock();
+
+//设置动画
+gsap.to(cube.position, { x: 5, duration: 5 });
+
 function render() {
-  renderer.render(scece, camera);
+  let time = clock.getElapsedTime();
+  console.log("时钟运行总时长", time);
+
+  // let deltaTime = clock.getDelta();
+  // console.log("两次获取时间的间隔时间", deltaTime);
+  let t = time;
+  cube.position.x = t * 1;
+  if (cube.position.x > 5) {
+    cube.position.x = 0;
+  }
+  renderer.render(scene, camera);
   //渲染下一帧的时候会调用render函数
   requestAnimationFrame(render);
 }
